a3c8a66fb2d36755dfe7e1225986f2887c15f179
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-02-25 08:46:23 -0800

    Create CustomSort.js
    Implement custom sort fn similar to sort prebuilt in arr

---
+++ CustomSort.js
@@ -0,0 +1,14 @@
+Array.prototype.customSort = function(callback){
+    for(let i=0;i<this.length;i++){
+        for(let j=0;j<this.length-1;j++){
+            if(this[j]-this[j+1]>0){
+                let temp = this[j];
+                this[j]=this[j+1];
+                this[j+1]=temp
+            }
+        }
+    }
+    return this
+}
+arr = [40,30,20,50,10]
+console.log(arr.customSort((a,b)=>a-b))
