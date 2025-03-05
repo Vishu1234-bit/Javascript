c1ec7beab5f3d84afac13cc8e4e1881b8f527536
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-03-05 03:07:43 -0800

    Update Custom_reduce.js
    with satisfying all test cases

--- Custom_reduce.js
+++ Custom_reduce.js
@@ -9,3 +9,23 @@ Array.prototype.customReduce = function(callback,initialValue){
 const arr1 = [4,5,3,2,1]
 const mappedarr1 = arr1.customReduce((acc,val)=>acc+val,0)
 console.log(mappedarr1)
+,................. With proper all test cases
+    /**
+ * @template T, U
+ * @param {(previousValue: U, currentValue: T, currentIndex: number, array: T[]) => U} callbackFn
+ * @param {U} [initialValue]
+ * @return {Array<U>}
+ */
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  if(initialValue===undefined && this.length===0){
+    throw new TypeError("Reduce of empty array with no initial value");
+  }
+     let accumulator = initialValue===undefined?this[0]:typeof this[0]==='string'?'':0;
+     let startIndex = initialValue===undefined?1:0;
+     for(let i=startIndex;i<this?.length;i++){
+      if(this[i]){
+      accumulator=callbackFn(accumulator,this[i],i,this)
+      }
+     }
+     return accumulator;
+};
