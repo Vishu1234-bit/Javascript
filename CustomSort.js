4dc77643a19d524a180c41d7ad3bbd7d1b40283d
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-03-13 05:27:52 -0700

    Update CustomSort.js

--- CustomSort.js
+++ CustomSort.js
@@ -1,7 +1,7 @@
 Array.prototype.customSort = function(callback){
     for(let i=0;i<this.length;i++){
         for(let j=0;j<this.length-1;j++){
-            if(this[j]-this[j+1]>0){
+            if(callback(this[j],this[j+1])>0){
                 let temp = this[j];
                 this[j]=this[j+1];
                 this[j+1]=temp
