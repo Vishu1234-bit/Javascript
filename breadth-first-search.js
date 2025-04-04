2ea28652748b1064b48fe3c67c2dd72458bce33c
Vishali <59252611+Vishu1234-bit@users.noreply.github.com>
2025-04-04 21:53:03 +0530

    Create breadth-first-search.js
    Write a function that implements the breadth-first search (BFS) algorithm on a directed graph (in adjacency list format), given a starting node.
    
    BFS is an algorithm used for traversing a graph or a tree, starting from the root node and exploring all the neighbors at the current depth before moving on to nodes at the next depth level. The output from BFS is an array of the graph's nodes in the order they were traversed. Visiting neighboring nodes in any order is a valid BFS, but for this question, please visit each node's neighbors from left to right.
    
    Examples
    const graph1 = {
      A: ['B', 'C', 'D'],
      B: ['E', 'F'],
      C: ['G', 'H'],
      D: ['I', 'J'],
      E: ['D'],
      F: [],
      G: [],
      H: [],
      I: [],
      J: [],
    };
    
    breadthFirstSearch(graph1, 'A'); // ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
    Illustration of graph1 from node 'A' 
    breadthFirstSearch(graph1, 'B'); // ['B', 'E', 'F', 'D', 'I', 'J']
    Illustration of graph1 from node 'B' 
    const graph2 = {
      A: ['B', 'C'],
      B: ['D', 'E'],
      C: ['F', 'G'],
      D: [],
      E: [],
      F: [],
      G: [],
    };
    
    breadthFirstSearch(graph2, 'A'); // ['A', 'B', 'C', 'D', 'E', 'F', 'G']
    Illustration of graph2 from node 'A' 
    breadthFirstSearch(graph2, 'E'); // ['E']
    A Queue data structure is also provided for you at the bottom of the skeleton code.
    
    Recap (Hint)
    Breadth-first search (BFS) is an algorithm used for traversing a graph or a tree. Here is an overview of how BFS works to traverse a graph, using the standard implementation that takes in an adjacency list (we use an array instead) and the root node:
    
    Initialize a queue to store nodes to be visited. Enqueue the root node.
    Initialize a set to track visited nodes.
    Enter a loop that continues until the queue is empty. In each iteration of the loop:
    Dequeue from the queue and mark it as visited.
    Retrieve the neighbors of the node from the input graph.
    For each neighbor, check if it has been visited. If it has not been visited, enqueue the node.
    Return the set of visited nodes.
    Breadth-first search is useful for the same purposes as Depth-first search, and it is especially useful for finding the shortest path between two nodes.

---
+++ breadth-first-search.js
@@ -0,0 +1,86 @@
+/**
+ * @param {Record<string, Array<string>} graph The adjacency list representing the graph.
+ * @param {string} source The source node to start traversal from. Has to be a valid node if graph is non-empty.
+ * @return {Array<string>} A BFS-traversed order of nodes.
+ */
+export default function breadthFirstSearch(graph, source) {
+  if(!graph||!graph[source]){
+    return [];
+  }
+  let queue = [source];
+  let visited = new Set();
+  let result = []
+  while(queue.length>0){
+    const current = queue.shift();
+    if(visited.has(current)){
+      continue;
+    }
+    visited.add(current);
+    result.push(current);
+    let neighbours = graph[current]||[];
+    for(let neighbour of neighbours){
+      if(!visited.has(neighbour)){
+        queue.push(neighbour);
+      }
+    }
+  }
+  return result;
+}
+
+/*  Auxiliary classes */
+
+/**
+ * A Queue class with O(1) enqueue and dequeue is provided for you.
+ * You can use it directly should you wish to.
+ *
+ * Example usage:
+ * const q = new Queue();
+ * q.enqueue('a');
+ * q.enqueue('b');
+ * q.dequeue(); //'a'
+ * q.isEmpty(); // False
+ */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  enqueue(item) {
+    const newNode = new Node(item);
+    if (this.isEmpty()) {
+      this.head = newNode;
+    } else if (this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    this.length++;
+  }
+
+  dequeue() {
+    if (this.isEmpty() || !this.head) {
+      return null;
+    } else {
+      const removedNode = this.head;
+      this.head = this.head.next;
+      removedNode.next = null;
+      this.length--;
+      if (this.isEmpty()) {
+        this.tail = null;
+      }
+      return removedNode.value;
+    }
+  }
+}
